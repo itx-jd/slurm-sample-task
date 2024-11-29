72c8bbd680b90534b8a3a415791da40fd240cedb
Muhammad Jawad <jawad2k01@gmail.com>
2024-11-29 16:05:07 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,11 +7,16 @@
 #SBATCH --time=00:10:00                  # Time limit (10 minutes)
 #SBATCH --partition=debug                # Partition name
 
+# Redirect all output to Slurm's error and output files
+exec >python_computation.out 2>python_computation.err
+
 # Load any necessary modules, if required (e.g., for virtualenv or Python)
 # module load python/3.8
 
 # Activate the virtual environment
 source venv/bin/activate
+
+pip install -r requirements.txt
 
 # Run the Python computation script
 python compute.py
